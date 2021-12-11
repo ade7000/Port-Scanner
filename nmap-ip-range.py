0d2eaf153d7c2ac2f05b501c318bb896977274e0
mihaid-b <mihaid_b@protonmail.com>
2021-12-11 18:46:46 +0200

    port range scanner

--- nmap-ip-range.py
+++ nmap-ip-range.py
@@ -4,22 +4,16 @@ import nmap
 begin = 75
 end = 80
 
-# assign the target ip to be scanned to
-# a variable
+
 target = '192.168.0.1'
 
-# instantiate a PortScanner object
+# folosesc nmap
 scanner = nmap.PortScanner()
 
 for i in range(begin, end + 1):
-    # scan the target port
     res = scanner.scan(target, str(i))
 
-    # the result is a dictionary containing
-    # several information we only need to
-    # check if the port is opened or closed
-    # so we will access only that information
-    # in the dictionary
+    # acesam dictionarul (key-value pairs)
     res = res['scan'][target]['tcp'][i]['state']
 
     print(f'port {i} is {res}.')
